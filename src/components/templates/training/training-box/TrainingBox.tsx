61576a55c1a7c8e2fafe16ff57ebf6dc761215d1
Denzel9 <denisnikitin9797@gmail.com>
2023-12-06 17:34:17 +0300

    12

--- src/components/templates/training/training-box/TrainingBox.tsx
+++ src/components/templates/training/training-box/TrainingBox.tsx
@@ -23,15 +23,49 @@ const TrainingBox: FunctionComponent<{ title: string; trainingList: string[] }> 
 
   const currentStep = steps?.slice(-1).join('')
 
+  const weights = exercisesSteps?.reduce((acc, cur) => (acc += cur.weight), 0)
+
+  const repeats = exercisesSteps?.reduce((acc, cur) => (acc += cur.repeat), 0)
+
+  const sets = exercisesSteps?.length
+
+  const progressBar = () => {
+    if (steps?.length === 1) return 'w-1/5'
+    if (steps?.length === 2) return ' w-2/5'
+    if (steps?.length === 3) return ' w-3/5'
+    if (steps?.length === 4) return ' w-4/5'
+    if (steps?.length === 5) return ' w-full'
+  }
   return (
     <div
       className={classNames(
-        start ? ' h-44' : ' h-16',
-        'duration-500 w-full p-2 bg-gold rounded-xl mt-10 relative py-4'
+        start ? ' h-60' : ' h-32',
+        'duration-500 w-full p-2 bg-white bg-opacity-40 rounded-xl mt-10 relative py-4'
       )}
       onClick={() => !start && setStart(true)}
     >
       <h2 className=" text-2xl">{title}</h2>
+
+      <div className=" flex gap-5">
+        <div>
+          <p>weights</p>
+          <p>{weights}</p>
+        </div>
+        <div>
+          <p>repeats</p>
+          <p>{repeats}</p>
+        </div>
+        <div>
+          <p>sets</p>
+          <p>{sets}</p>
+        </div>
+      </div>
+
+      <div className=" h-2 mt-2 w-full bg-base rounded-full relative">
+        <div
+          className={classNames(progressBar(), 'absolute left-0 top-0 bg-gold h-2 rounded-full')}
+        ></div>
+      </div>
 
       <div className={classNames(edit ? ' opacity-0 duration-300' : ' opacity-100 duration-300')}>
         {start && <p className=" pb-2">Подходы:</p>}
